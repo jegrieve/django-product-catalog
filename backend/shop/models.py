ec3ee21062fc525bde9a246fd0cc7a91a3cbc535
James Grieve <28887182+jegrieve@users.noreply.github.com>
2025-10-01 11:18:11 -0700

    feat(models): add tag  model

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -8,3 +8,9 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
