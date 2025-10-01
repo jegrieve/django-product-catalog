13de98e3209fbc18649efd25507eb17edaabbca6
James Grieve <28887182+jegrieve@users.noreply.github.com>
2025-10-01 11:25:47 -0700

    feat(models): add product model

--- backend/shop/models.py
+++ backend/shop/models.py
@@ -14,3 +14,23 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True,
+        related_name="products",
+    )
+
+    def __str__(self):
+        return self.name
