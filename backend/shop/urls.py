7e76f301b54f577f3faf8756a6be99d1551fbd3f
James Grieve <28887182+jegrieve@users.noreply.github.com>
2025-10-01 12:40:44 -0700

    feat(api): add basic GET api/products endpoint to get all products

--- backend/shop/urls.py
+++ backend/shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("categories", views.categories_api, name="categories_api"),
     path("tags", views.tags_api, name="tags_api"),
+    path("products", views.products_api, name="products_api"),
 ]
